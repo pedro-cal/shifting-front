1b5c7bb9423693b004d3d1d6b2967aec6989010f
admin <admin@MacBook-Air-de-admin.local>
2023-11-13 17:09:32 -0300

    feat: (wip) gradient bg transitions

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image"
-import logoBeta from "/logoBeta.jpg"
+// import Image from "next/image"
+// import logoBeta from "/logoBeta.jpg"
 
 export default function Header() {
   return (
     <>
-      <header className="z-20 flex h-[10vh] flex-col bg-stone-900 p-3 px-8 sm:flex-row lg:h-[10vh]">
-        <div className="w-1/2">
+      <header className="absolute top-0 left-0 z-20 flex w-full h-[10vh] flex-col bg-none p-3 px-8 sm:flex-row lg:h-[10vh]">
+        {/* <div className="absolute -top-5 -left-5 z-20 flex w-[110vw] h-[20vh] flex-col blur bg-stone-300 opacity-10 p-3 px-8 sm:flex-row lg:h-[10vh]" /> */}
+        <div className="z-30 w-1/2">
           <button className="flex h-full flex-row place-content-center items-center">
-            <Image className="aspect-square h-full w-full" src={logoBeta} alt="Logo of Shifting" />
-            <p className="pl-2 text-stone-50 lg:text-3xl 2xl:pl-4 2xl:text-3xl">Shifting</p>
+            {/* <Image className="aspect-square h-full w-full" src={logoBeta} alt="Logo of Shifting" /> */}
+            <p className="pl-2 text-stone-50 font-black lg:text-3xl 2xl:pl-4 2xl:text-3xl">Shifting</p>
           </button>
         </div>
-        <div className="my-2 flex h-full place-content-center items-center space-x-4 md:my-0 md:mr-2 md:w-1/2 md:place-content-end ">
+        <div className="z-30 my-2 flex h-full place-content-center items-center space-x-4 md:my-0 md:mr-2 md:w-1/2 md:place-content-end ">
           <button className="flex h-4/5 place-content-center items-center p-2 font-sans text-xl text-stone-50 hover:text-sky-500">
             Portfolio
           </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import Form from "components/Form/Form"
 import Header from "components/Header/Header"
 import SectionOne from "components/SectionOne/SectionOne"
@@ -8,12 +8,63 @@ import SectionOne from "components/SectionOne/SectionOne"
 const imgUrl = "/images/vegetables.jpg"
 
 export default function Home() {
-  const draggableRef = useRef<HTMLDivElement>(null)
+  const draggableRef = useRef<HTMLDivElement>(null);
+  const bgAngle = useState<number>(90);
 
   useEffect(() => {
-    document.onmousemove = (e) => handleOnMove(e)
+    document.onmousemove = (e) => handleOnMove(e);
     // document.ontouchmove = (e) => handleOnMove(e.touches[0]);
   }, [])
+  useEffect(() => {
+    handleBackgroundChanges();
+    // document.ontouchmove = (e) => handleOnMove(e.touches[0]);
+  }, [])
+
+  function handleBackgroundChanges() {
+    const elementId = 'gradient-button-transition'; // <-- id of the button we're transitioning
+
+    // DEFINE YOUR GRADIENT COLORS HERE
+    // Pct refers to the percentage position of the gradient stop point.
+    const gradientStopOne = [
+        { pct: 0,  color: { r: 255, g: 113, b: 71 } }, // The first color in your gradient
+        { pct: 100, color: { r: 0, g: 55, b: 255 } }   // The color you want your first color to transition to
+    ];
+    const gradientStopTwo = [
+        { pct: 0,  color: { r: 224, g: 65, b: 127 } }, // The second color in your gradient
+        { pct: 100, color: { r: 0, g: 173, b: 255 } }  // The color you want your second color to transition to
+    ]
+
+    // Apply our gradient programmatically so we can completely manipulate the gradient from JS rather than CSS
+    const c1 = gradientStopOne[0].color;
+    const c2 = gradientStopTwo[0].color;
+    const bgElement = document.getElementById(elementId);
+    if (bgElement) {
+      bgElement.style.background = `linear-gradient(${bgAngle}deg, rgb(${c1.r}, ${c1.g}, ${c1.b}), rgb(${c2.r}, ${c2.g}, ${c2.b}))`;
+    }
+      
+    // This function transitions between two rgb colors
+    // const getColor = function(pct, colorSet) {
+    //     for (var i = 1; i < colorSet.length - 1; i++) {
+    //         if (pct < colorSet[i].pct) {
+    //             break;
+    //         }
+    //     }
+    //     // This conversion figures out the transition between two rgb values
+    //     const lower = colorSet[i - 1];
+    //     const upper = colorSet[i];
+    //     const range = upper.pct - lower.pct;
+    //     const rangePct = (pct - lower.pct) / range;
+    //     const pctLower = 1 - rangePct;
+    //     const pctUpper = rangePct;
+    //     const color = {
+    //         r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+    //         g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+    //         b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+    //     };
+    //     // And returns the rgb code
+    //     return `rgb(${color.r}, ${color.g}, ${color.b})`;
+    // }
+  }
 
   const handleOnMove = (e: MouseEvent) => {
     const position = (e.clientX / window.innerWidth) * 100
@@ -43,7 +94,8 @@ export default function Home() {
       <section>
         <SectionOne />
       </section>
-      <section className="min-h-screen bg-gray-800 text-white">
+
+      <section className="section-background min-h-screen bg-gray-800 text-white">
         <div
           id="left-side"
           className="absolute z-10 grid h-[100vh] w-full place-items-center overflow-hidden bg-sky-700"
