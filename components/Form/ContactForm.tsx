34d9c5925eaf9ce5a40669a8ba84da93d5c54a93
Diego Almeida <diegoalmeida1985@gmail.com>
2023-11-22 19:14:25 -0300

    Feat: Form fields validated

--- components/Form/ContactForm.tsx
+++ components/Form/ContactForm.tsx
@@ -1,6 +1,9 @@
 import * as Form from '@radix-ui/react-form'
 import { useState } from "react"
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import InputField from "./InputField"
+
 
 export default function ContactForm() {
   const contacts = [
@@ -17,27 +20,71 @@ export default function ContactForm() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-
+  const [firstNameIsValid, setFirstNameIsValid] = useState(true);
+  const [lastNameIsValid, setLastNameIsValid] = useState(true);
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [phoneNumberIsValid, setPhoneNumberIsValid] = useState(true);
+  const [patternIsValid, setPatternIsValid] = useState(true)
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let counter = 4;
+  
   function handleSubmit() {
-    const addNewContact = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phoneNumber: phoneNumber,
+    if(patternIsValid === true){
+      console.log(email+' is a Valid Email')
+    }else{console.log(email+' is a Invalid Email')}
+
+    if(firstName === ''){
+      setFirstNameIsValid(false)
+      counter -= 1
+    }else{setFirstNameIsValid(true)}
+
+    if(lastName === ''){
+      setLastNameIsValid(false)
+      counter -= 1
+    }else{setLastNameIsValid(true)}
+
+    if(patternIsValid === false){
+      setEmailIsValid(false)
+      counter -= 1
+    }else{setEmailIsValid(true)}
+    console.log(emailIsValid)
+
+    if(phoneNumber === '' || phoneNumber.length < 7){
+      setPhoneNumberIsValid(false)
+      counter -= 1
+    }else{setPhoneNumberIsValid(true)}
+
+    if(counter === 4){
+      const addNewContact = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber,
+      }
+      const contactListUpdated = [...contactList, addNewContact]
+      setContactList(contactListUpdated)
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPhoneNumber("");
+      setFirstNameIsValid(true);
+      setLastNameIsValid(true);
+      setEmailIsValid(true);
+      setPhoneNumberIsValid(true);
     }
-    const contactListUpdated = [...contactList, addNewContact]
-    setContactList(contactListUpdated)
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPhoneNumber("");
+    
+    console.log(counter)
+    console.log("ContactList: ")
+    console.log(contactList)
   }
+    
   console.log("ContactList: ")
   console.log(contactList)
+  
 
   return (
     <>
-      <section className="flex h-screen flex-col content-center items-center bg-stone-950 md:flex-row-reverse">
+      <section className="flex h-screen w-screen overflow-hidden flex-col content-center items-center bg-stone-950 md:flex-row-reverse">
         <div className="my-8 flex items-center justify-center md:w-1/2">
           <p className="flex w-3/4 text-center text-xl text-stone-50 md:w-1/2 md:text-2xl lg:text-3xl">
             Do you want a personalized solution for your business? Fill out our form now and our team will contact you.
@@ -58,6 +105,11 @@ export default function ContactForm() {
                 value={firstName}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setFirstName(event.target.value)}
               />
+              {firstNameIsValid === false ? (
+                <div className="text-[13px] text-end pt-2 text-white opacity-[0.8]">
+                  Please enter your First Name
+                </div>
+              ):null}
             </Form.Field>
             <Form.Field className="grid mb-[10px]" name="lname">
               <div className="flex items-baseline justify-between">
@@ -72,23 +124,33 @@ export default function ContactForm() {
                 value={lastName}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLastName(event.target.value)}
               />
+              {lastNameIsValid === false ? (
+                <div className="text-[13px] text-end pt-2 text-white opacity-[0.8]">
+                  Please enter your Last Name
+                </div>
+              ):null}
             </Form.Field>
             <Form.Field className="grid mb-[10px]" name="email">
               <div className="flex items-baseline justify-between">
                 <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                   Email
                 </Form.Label>
-                <Form.Message className="text-[13px] text-white opacity-[0.8]" match="typeMismatch">
-                  Please provide a valid Email
-                </Form.Message>
               </div>
               <InputField
                 type = "text"
                 name = "email"
                 placeHolder="Email"
                 value={email}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setEmail(event.target.value);
+                  setPatternIsValid(emailPattern.test(email))
+                }}
               />
+              {emailIsValid === false ? (
+                <div className="text-[13px] text-end pt-2 text-white opacity-[0.8]">
+                  Please enter a valid Email
+                </div>
+              ):null}
             </Form.Field>
             <Form.Field className="grid mb-[10px]" name="phoneNumber">
               <div className="flex items-baseline justify-between">
@@ -96,28 +158,35 @@ export default function ContactForm() {
                   Phone Number
                 </Form.Label>
               </div>
-              <InputField
-                type = "text"
-                name = "phoneNumber"
-                placeHolder="Phone Number"
+              <PhoneInput
+                containerClass="box-border w-full bg-stone-900 shadow-stone-400 inline-flex h-[35px] rounded-lg text-[15px] leading-none text-stone-50 placeholder:text-stone-700 shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_2px_#a8a29e] focus:shadow-[0_0_0_2px_#a8a29e] selection:text-stone-50 selection:bg-stone-950"
+                inputClass="box-border w-full bg-stone-900 shadow-transparent inline-flex h-[35px] items-center justify-center px-[10px] text-[15px] leading-none text-stone-50 placeholder:text-stone-700 shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_2px_#a8a29e] focus:shadow-[0_0_0_2px_#a8a29e] selection:text-stone-50 selection:bg-stone-950"
+                dropdownStyle={{backgroundColor: '#0c0a09', color: '#57534e'}}
+                buttonStyle={{marginLeft: '0px',backgroundColor: '#1c1917', border: 'solid 2px transparent', borderRadius: '0.5rem'}}
+                buttonClass='hover:bg-stone-900 focus:bg-stone-900'
+                inputStyle={{backgroundColor: '#1c1917', color: '#fafaf9', width: '100%', height: '100%', border: 'solid 2px transparent', borderRadius: '0.5rem'}}
+                country="us"
+                placeholder='Phone Number'
                 value={phoneNumber}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhoneNumber(event.target.value)}
+                onChange={(phoneNumber) => setPhoneNumber(phoneNumber)}
               />
-              <Form.Message className="text-[13px] text-center md:text-right text-white opacity-[0.8]" match="valueMissing">
-                Please enter your Phone Number
-              </Form.Message>
-              <Form.Message className="text-[13px] text-center md:text-right text-white opacity-[0.8]" match="typeMismatch">
-                Please provide a valid Phone Number
-              </Form.Message>
+              {phoneNumberIsValid === false ? (
+                <div className="text-[13px] text-end pt-2 text-white opacity-[0.8]">
+                  Please enter a valid Phone Number
+                </div>
+              ):null}
             </Form.Field>
             <Form.Submit asChild>
-                <div className='w-full flex items-center justify-center'>
-                  <button onClick={handleSubmit} className="box-border w-full lg:w-2/3 bg-sky-700 text-sky-50 shadow-sky-950 hover:bg-sky-600 hover:text-black flex h-[35px] items-center justify-center rounded-lg px-[15px] font-medium leading-none shadow-[0_2px_5px] focus:shadow-[0_0_0_2px] focus:outline-none mt-[20px]">
-                    Submit
-                  </button>
-                </div>
+              <div className='w-full flex items-center justify-center'>
+                <button onClick={handleSubmit} className="box-border w-full lg:w-2/3 bg-sky-700 text-sky-50 shadow-sky-950 hover:bg-sky-600 hover:text-black flex h-[35px] items-center justify-center rounded-lg px-[15px] font-medium leading-none shadow-[0_2px_5px] focus:shadow-[0_0_0_2px] focus:outline-none mt-[20px]">
+                  Submit
+                </button>
+              </div>
             </Form.Submit>
           </Form.Root>
+          {/* <button onClick={handleSubmit} className="box-border w-full lg:w-2/3 bg-sky-700 text-sky-50 shadow-sky-950 hover:bg-sky-600 hover:text-black flex h-[35px] items-center justify-center rounded-lg px-[15px] font-medium leading-none shadow-[0_2px_5px] focus:shadow-[0_0_0_2px] focus:outline-none mt-[20px]">
+            Submit
+          </button> */}
         </div>
       </section>
     </>
--- components/Form/InputField.tsx
+++ components/Form/InputField.tsx
@@ -16,7 +16,8 @@ function InputField(props: ContactData) {
         
         <Form.Control asChild>
             <input
-                className="mb-2 p-2 flex w-full rounded-lg bg-stone-900 placeholder:text-base placeholder:text-stone-700 text-stone-50 border-solid border-2 border-stone-400 focus:ring-2 focus:ring-stone-500"
+                //className="mb-2 p-2 flex w-full rounded-lg bg-stone-900 placeholder:text-base placeholder:text-stone-700 text-stone-50 border-solid border-2 border-stone-400 focus:ring-0 focus:ring-transparent"
+                className="box-border w-full bg-stone-900 shadow-stone-400 inline-flex h-[35px] items-center justify-center rounded-lg px-[10px] leading-none text-stone-50 placeholder:text-base placeholder:text-stone-700 shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_2px_#a8a29e] focus:shadow-[0_0_0_2px_#a8a29e] selection:text-stone-50 selection:bg-stone-950"
                 required
                 autoComplete="off"
                 type={type}
